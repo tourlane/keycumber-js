c9731430e0ecf09f92a0bbd5866312ac6c61a0ab
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2021-09-13 15:15:16 +0300

    chore: format with prettier

--- main.js
+++ main.js
@@ -1,50 +1,68 @@
-import { fromEvent, combineLatest } from 'https://cdn.skypack.dev/rxjs'
-import { debounceTime, filter, map, startWith } from 'https://cdn.skypack.dev/rxjs/operators'
-import _chunk from 'https://cdn.skypack.dev/lodash.chunk'
+import { fromEvent, combineLatest } from "https://cdn.skypack.dev/rxjs";
+import {
+  debounceTime,
+  filter,
+  map,
+  startWith,
+} from "https://cdn.skypack.dev/rxjs/operators";
+import _chunk from "https://cdn.skypack.dev/lodash.chunk";
 
-let fromCsvTextArea = ($el) => fromEvent($el, 'input').pipe(
-  debounceTime(300),
-  map(e => e.target.value.split('\n').map(s => s.trim()).filter(Boolean)),
-  filter(v => v.length > 0),
-)
+let fromCsvTextArea = ($el) =>
+  fromEvent($el, "input").pipe(
+    debounceTime(300),
+    map((e) =>
+      e.target.value
+        .split("\n")
+        .map((s) => s.trim())
+        .filter(Boolean)
+    ),
+    filter((v) => v.length > 0)
+  );
 
-let $mode = document.getElementById('mode')
-let $result = document.getElementById('result')
+let $mode = document.getElementById("mode");
+let $result = document.getElementById("result");
 
 combineLatest([
-  fromCsvTextArea(document.getElementById('destinations')),
-  fromCsvTextArea(document.getElementById('mods')),
-  fromEvent($mode, 'input').pipe(map(e => e.target.value), startWith($mode.value)),
-]).pipe(
-  map(([destinations, mods, mode]) =>
-    destinations.flatMap(d => mods.flatMap(m => {
-      switch (mode) {
-        case 'dst-first':
-          return `${d} ${m}`
-        case 'mod-first':
-          return `${m} ${d}`
-        default:
-          return [`${d} ${m}`, `${m} ${d}`]
-      }
-    })),
+  fromCsvTextArea(document.getElementById("destinations")),
+  fromCsvTextArea(document.getElementById("mods")),
+  fromEvent($mode, "input").pipe(
+    map((e) => e.target.value),
+    startWith($mode.value)
   ),
-).subscribe(r => $result.value = r.join('\n'))
+])
+  .pipe(
+    map(([destinations, mods, mode]) =>
+      destinations.flatMap((d) =>
+        mods.flatMap((m) => {
+          switch (mode) {
+            case "dst-first":
+              return `${d} ${m}`;
+            case "mod-first":
+              return `${m} ${d}`;
+            default:
+              return [`${d} ${m}`, `${m} ${d}`];
+          }
+        })
+      )
+    )
+  )
+  .subscribe((r) => ($result.value = r.join("\n")));
 
-
-let toDataCsv = (values) => encodeURI('data:text/csv;charset=utf-8,' + values.join('\n'))
+let toDataCsv = (values) =>
+  encodeURI("data:text/csv;charset=utf-8," + values.join("\n"));
 
 let download = (fileName, content) => {
-  let link = document.createElement('a')
-  link.setAttribute('href', content)
-  link.setAttribute('download', fileName)
-  document.body.appendChild(link)
-  link.click()
-}
+  let link = document.createElement("a");
+  link.setAttribute("href", content);
+  link.setAttribute("download", fileName);
+  document.body.appendChild(link);
+  link.click();
+};
 
-let $itemsPerFile = document.getElementById('itemsPerFile')
+let $itemsPerFile = document.getElementById("itemsPerFile");
 
-document.getElementById('download').addEventListener('click', () => {
-  _chunk($result.value.split('\n'), +$itemsPerFile.value)
-    .forEach((values, i) => download(`my_data-${i}.csv`, toDataCsv(values)))
-
-})
+document.getElementById("download").addEventListener("click", () => {
+  _chunk($result.value.split("\n"), +$itemsPerFile.value).forEach((values, i) =>
+    download(`my_data-${i}.csv`, toDataCsv(values))
+  );
+});
